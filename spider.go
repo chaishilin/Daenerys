82bcf99be775a99f758b9901c80886d19607a394
chaishilin <chaishilin@outlook.com>
2020-05-09 23:01:36 +0800

    spider jingdong

--- spider.go
+++ spider.go
@@ -3,53 +3,95 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 func main() {
-	end := make(chan int)
 	url := "https://www.jd.com/allSort.aspx"
-	urllist := []string{url}
-	count := 0
-	for _, v := range urllist {
-		go visitURL(v, end)
-		count++
-	}
-	if count > 0 {
-		for range end {
-			count--
-			if count == 0 {
-				break
-			}
-		}
-	}
+	//url := "https://movie.douban.com/tag/#/?sort=T&tags=%E8%B6%85%E7%BA%A7%E8%8B%B1%E9%9B%84"
+	visitURL(url)
 	fmt.Println("the end ")
 }
 
-func visitURL(url string, end chan int) {
-	defer func() { end <- 1 }()
+func visitURL(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch :%v\n", err)
+		fmt.Fprintf(os.Stderr, "fetch get %v", err)
 		os.Exit(1)
 	}
-	b, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch : reading %v\n", err)
-		os.Exit(1)
-	}
-
-	node, err := html.Parse(resp.Body)
-
+	doc, err := html.Parse(resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch : reading %v\n", err)
+		fmt.Fprintf(os.Stderr, "fetch parse%v", err)
 		os.Exit(1)
 	}
-	fmt.Printf("%v\n", node)
-	fmt.Printf("get : %s  %s\n", url, b[:10])
+	getURL(doc)
+}
 
+func getURL(n *html.Node) {
+	parseForm(n)
+	//do dfs
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		getURL(c)
+	}
+}
+
+func parseForm(n *html.Node) {
+	nodeList := []*html.Node{}
+	result := haveLabel(n, []string{"category-item m", "mt", "item-title", "span"}, &nodeList)
+	if result == true {
+		fmt.Println("\n----------------------------------------------\n")
+		fmt.Printf("大类: %s\t\n", nodeList[0].FirstChild.Data)
+	}
+
+	nodeList = []*html.Node{}
+	result = haveLabel(n, []string{"category-item m", "mc", "items", "dl", "dt", "a"}, &nodeList)
+
+	if result == true {
+		for _, dtNode := range nodeList {
+			fmt.Printf("--中类: %s\t", dtNode.FirstChild.Data)
+			fmt.Printf("链接: %s \n", dtNode.Attr[0].Val)
+			ddNode := dtNode.Parent.Parent
+			for q := ddNode.FirstChild; q != nil; q = q.NextSibling {
+				if q.Data == "dd" {
+					for c := q.FirstChild; c != nil; c = c.NextSibling {
+						if c.Data == "a" {
+							fmt.Printf("----小类: %s\t", c.FirstChild.Data)
+							fmt.Printf("链接: %s \n", c.Attr[0].Val)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func haveLabel(n *html.Node, labelList []string, nodeList *[]*html.Node) bool {
+	if len(labelList) == 1 {
+		if n.Data == labelList[0] {
+			*nodeList = append(*nodeList, n)
+			return true
+		}
+		return false
+	}
+	flag := 0
+	for _, v := range n.Attr {
+		if v.Key == "class" && v.Val == labelList[0] {
+			flag = 1
+		}
+	}
+	if n.Data == labelList[0] {
+		flag = 1
+	}
+	if flag == 1 {
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			haveLabel(c, labelList[1:], nodeList)
+		}
+	}
+	if len(*nodeList) > 0 {
+		return true
+	} else {
+		return false
+	}
 }
