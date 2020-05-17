c447e48de7b70c3f833b33ef80172d2d40b8c759
chaishilin <chaishilin@outlook.com>
2020-05-17 11:10:20 +0800

    字段查询

--- web.go
+++ web.go
@@ -11,31 +11,46 @@ import (
 
 type classes struct {
 	Name string
-	Id int
-	Pid int
+	Id   int
+	Pid  int
 	Href string
 }
+
+const (
+	queryById = `select a.class_id,a.class_name,a.class_href 
+					from classTable as a join classRelate as b 
+					where a.class_id = b.class_id and 
+					(b.pid = ? or b.class_id = ?);`
+	queryByName = `select a.class_id,a.class_name,a.class_href 
+						from classTable as a join classRelate as b 
+						where a.class_id = b.class_id and 
+						a.class_name regexp '%s';`
+)
 
 func classHandler(w http.ResponseWriter, r *http.Request) {
 
 	classList := []classes{}
 	r.ParseForm()
-	qId := r.Form.Get("input")
+
 	conn := sqlgo.InitMySql()
 	//findAll := "select * from classTable"
-	findClassId := `select a.class_id,a.class_name,a.class_href 
-					from classTable as a join classRelate as b 
-					where a.class_id = b.class_id and 
-					(b.pid = ? or b.class_id = ?);`
-	result := sqlgo.SelectSql(conn,findClassId,qId,qId)
-	for _,v := range result{
-		intId,_ := strconv.Atoi(v[0])
-		classList = append(classList,classes{Id: intId,Name: v[1],Href: v[2]})
+
+	query := r.Form.Get("input")
+	_, err := strconv.Atoi(query)
+	var result [][]string
+	if err != nil {
+		if len(query) > 0{
+		result = sqlgo.SelectSql(conn, fmt.Sprintf(queryByName,fmt.Sprintf(".*%s.*",query)))//"'."+query+".'"
+		}
+	} else {
+		result = sqlgo.SelectSql(conn, queryById, query, query)
+	}
+	for _, v := range result {
+		intId, _ := strconv.Atoi(v[0])
+		classList = append(classList, classes{Id: intId, Name: v[1], Href: v[2]})
 		//fmt.Printf("id : %s ,名字 :  %s,链接 : %s \n",v[0],v[1],v[2])
 	}
-	/*
-	如何通过post请求，得到希望查询的商品分类信息？
-	 */
+
 
 	t, err := template.ParseFiles("./root/hello.html")
 	if err != nil {
@@ -49,12 +64,11 @@ func classHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", classHandler)
 
-	server := &http.Server {
-		Addr:    ":8080",
+	server := &http.Server{
+		Addr:    ":18080",
 		Handler: mux,
 	}
 	if err := server.ListenAndServe(); err != nil {
