ad0ccb74fc8ab1ece2df340e218834ea3c943c15
chaishilin <chaishilin@outlook.com>
2020-05-17 10:01:56 +0800

    simple web demo

--- web.go
+++ web.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"./sqlgo"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type classes struct {
@@ -14,26 +16,42 @@ type classes struct {
 	Href string
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func classHandler(w http.ResponseWriter, r *http.Request) {
+
+	classList := []classes{}
+	r.ParseForm()
+	qId := r.Form.Get("input")
+	conn := sqlgo.InitMySql()
+	//findAll := "select * from classTable"
+	findClassId := `select a.class_id,a.class_name,a.class_href 
+					from classTable as a join classRelate as b 
+					where a.class_id = b.class_id and 
+					(b.pid = ? or b.class_id = ?);`
+	result := sqlgo.SelectSql(conn,findClassId,qId,qId)
+	for _,v := range result{
+		intId,_ := strconv.Atoi(v[0])
+		classList = append(classList,classes{Id: intId,Name: v[1],Href: v[2]})
+		//fmt.Printf("id : %s ,名字 :  %s,链接 : %s \n",v[0],v[1],v[2])
+	}
+	/*
+	如何通过post请求，得到希望查询的商品分类信息？
+	 */
+
 	t, err := template.ParseFiles("./root/hello.html")
 	if err != nil {
 		w.WriteHeader(500)
 		fmt.Fprint(w, err)
 		return
 	}
-	classlist := []classes{
-		{Name: "数码产品",Id: 1,Pid: 0,Href: "www.shuma.com"},
-		{Name: "手机",Id: 2,Pid: 1,Href: "https://channel.jd.com/shouji.html"},
-		{Name: "苹果手机",Id: 3,Pid: 1,Href: "https://list.jd.com/list.html?cat=652,829,845"},
-		{Name: "相机",Id: 4,Pid: 1,Href: "www.camera.com"},
-		{Name: "索尼相机",Id: 5,Pid: 4,Href: "www.sonycamera.com"},
-	}
-	t.Execute(w, classlist)
+
+	t.Execute(w, classList)
 }
 
 func main() {
+
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/", classHandler)
 
 	server := &http.Server {
 		Addr:    ":8080",
