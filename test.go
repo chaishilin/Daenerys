07e953f2f8ff7e2fd0bc92f81d93d84b8d5ad478
chaishilin <chaishilin@outlook.com>
2020-05-21 20:11:44 +0800

    goods spider

--- test.go
+++ test.go
@@ -2,32 +2,13 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
+	"io/ioutil"
+	"net/http"
 )
 
-func main() {
-	id := 0
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	for i:=0;i<10;i++{
-		mu.Lock()
-		fmt.Printf("pid:无, id:%d\n",id)
-		id++
-		time.Sleep(10*time.Microsecond)
-		mu.Unlock()
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			for j := 0; j < 10; j++ {
-				mu.Lock()
-				fmt.Printf("pid:%d, id:%d\n",i,id)
-				id++
-				time.Sleep(10*time.Microsecond)
-				mu.Unlock()
-			}
-		}(i)
-	}
-	wg.Wait()
-	fmt.Println("should be :",30*200,id)
+func main(){
+	href :="//item.jd.com/3650111.html"
+	resp,_:=http.Get(href)
+	bBody,_ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(bBody))
 }
